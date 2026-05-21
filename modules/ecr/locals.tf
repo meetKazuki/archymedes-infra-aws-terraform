58fa70c917013260b98aa2188dcb894d5524c056
Desmond Edem <edemintech@gmail.com>
2026-05-21 04:42:32 +0100

    fix: fix ecr config

--- modules/ecr/locals.tf
+++ modules/ecr/locals.tf
@@ -1,5 +1,5 @@
 locals {
   repository_name_parsed = replace(lower(var.repository_name), " ", "-")
 
-  effective_principals = length(var.allowed_principal_arns) > 0 ? var.allowed_principal_arns : [data.aws_caller_identity.current.arn]
+  effective_principals = length(var.allowed_principal_arns) > 0 ? var.allowed_principal_arns : [data.aws_iam_session_context.current.issuer_arn]
 }
